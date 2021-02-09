a0c97ac2a6a0dbfee4266363ac4d72f98cdabaa7
ihsanmohamad <ihsanmohamad13@gmail.com>
2021-02-10 04:52:06 +0800

    _redirects

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,8 +1,12 @@
 import './tailwind.css'
 import './style.css'
 
+// import {app , router, siteData} from 'vitepress'
 import Layout from './Layout.vue'
 
 export default {
-  Layout
+  Layout,
+  NotFound: () => 'custom 404', // <- this is a Vue 3 functional component
+  enhanceApp({ app, router, siteData }) {
+  }
 }
